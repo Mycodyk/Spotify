086aa6bc6bcb9c34fee8f2a0f7ea969da0331735
Максим Федоров <129328321+Mycodyk@users.noreply.github.com>
2024-10-13 21:04:31 +0300

    create modal, modalprovider and more

---
+++ components/Modal.tsx
@@ -0,0 +1,10 @@
+import * as Dialog from "@radix-ui/react-dialog"
+import { IoMdClose } from "react-icons/io"
+
+export const Modal = () => {
+    return(
+        <div>
+            Modals component
+        </div>
+    )
+}
