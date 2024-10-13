1ad43ba64198d9cd5215552a7209843adb87a0a3
Максим Федоров <129328321+Mycodyk@users.noreply.github.com>
2024-10-13 20:16:42 +0300

    add structure repo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Figtree } from 'next/font/google'
 import { Sidebar } from "@/components/Sidebar";
-import { SupabaseProvider } from "./providers/SupaBase";
+import SupabaseProvider from "@/providers/Supabase";
 
 const font = Figtree({ subsets: ['latin']})
 
